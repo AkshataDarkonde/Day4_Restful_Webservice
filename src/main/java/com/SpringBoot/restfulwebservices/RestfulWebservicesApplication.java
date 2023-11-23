8b0cf0c483f5ada197b6d47d4168563151dd3bac
adarkond <akshatad1234@gmail.com>
2023-11-23 16:29:32 +0530

    "Connecting database"

---
+++ src/main/java/com/SpringBoot/restfulwebservices/RestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
